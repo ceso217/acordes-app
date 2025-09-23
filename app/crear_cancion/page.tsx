82ba6f7c109e17f30c8f80d00ba526355f2c0099
ceso217 <baronicecilio@gmail.com>
2025-09-23 20:53:30 -0300

    crear cancion update

--- app/crear_cancion/page.tsx
+++ app/crear_cancion/page.tsx
@@ -1,34 +1,57 @@
+"use client";
+
+import { use } from "react";
+import {useForm} from "react-hook-form";
+import axios from "axios";
+
+type Form = { nombre: string, autor: string, letra: string, referencia: string };
+
 export default function CrearCancionesPage() {
+    const {register, handleSubmit, setError, formState: {errors, isSubmitting} } = useForm<Form>();
+    
+    const onSubmit = async (data:Form) => {
+        try{
+            const res = await axios.post("/api/canciones", data);
+
+            console.log("Respuesta del servidor:", res.data);
+        } catch(error: any) {
+            if(error.response){
+                setError()
+            }
+
+        }
+    }
+    
     return(
-    <div className="h-screen flex items-center bg-[#D7F5DC] ">
-        <div className="flex flex-col items-center m-auto my-20 p-9 bg-amber-200 rounded-3xl w-1/2 h-1/2 text-lg">
+    <div className="h-screen flex items-center bg-[#385b3e] ">
+        <div className="flex flex-col items-center mx-10 p-9 pb-0 bg-emerald-400 rounded-3xl w-full h-auto text-lg">
             <div className="my-2 w-full ">
                 <label className="flex">
-                    Título:
+                    Nombre:
                     <input className="bg-white mx-4 w-full rounded-xl px-3" type="text"/>
                 </label>
             </div>
             <div className="my-2 w-full">
                 <label className="flex">
-                Autor:
-                <input className="bg-white mx-4 w-full rounded-xl" type="text"/>
+                    Autor:
+                    <input className="bg-white mx-4 w-full rounded-xl" type="text"/>
+                <button className="bg-white rounded-4xl px-3">+</button>
             </label>
             </div>
             <div className="my-2 w-full">
                 <label className="flex ">
-                Canción:
-                <textarea className="bg-white mx-4 w-full rounded-xl resize-none" rows={4}/>
+                    Letra:
+                    <textarea className="bg-white mx-4 w-full rounded-xl resize-none" rows={8}/>
             </label>
             </div>
-            <div className="my-2 w-full">
+            <div className="mt-2 w-full">
                 <label className="flex">
-                Referencia:
-                <input className="bg-white mx-4 w-full rounded-xl" type="text"/>
+                    Referencia:
+                    <input className="bg-white mx-4 w-full rounded-xl" type="text"/>
                 </label>
             </div>
-            <button type="submit" className="p-2 bg-green-500 rounded-4xl">Agregar canción</button>
+            <button type="submit" className="p-2 bg-green-500 rounded-4xl m-6">Agregar canción</button>
         </div>
     </div>
-        
     )
 }
